e8fcba091e61b2ae96457a1ba41b3d397ceb7a25
Marcos Vinicius <marvn.ants1@gmail.com>
2020-05-08 20:19:58 -0300

    feat: carregando models de forma automatizada

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize';
+import { join } from 'path';
+import { readdirSync } from 'fs';
 
 import dbConfig from '../config/database';
 
@@ -15,6 +17,22 @@ class Database {
     } catch (error) {
       console.error('Unable to connect to the database: ', error);
     }
+
+    this.loadModels().map((obj) =>
+      Object.values(obj).map((model) => model.init(this.connection))
+    );
+
+    return this.connection;
+  }
+
+  loadModels() {
+    const dir = join(__dirname, '../models');
+    let models = [];
+    readdirSync(dir).forEach((file) => {
+      const modelPath = require(dir + '/' + file);
+      models.push(modelPath);
+    });
+    return models;
   }
 }
 
