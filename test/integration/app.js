f881c249872ae803e2a89154362e2fee87b75dc5
Marcos Vinicius <marvn.ants1@gmail.com>
2020-05-08 20:16:42 -0300

    test: adicionando testes

--- test/integration/app.js
+++ test/integration/app.js
@@ -1,16 +1,44 @@
-describe('Routes User', () => {
+import { expect } from 'chai';
+import supertest from 'supertest';
+
+import conn from '../../src/database';
+import Users from '../../src/models/user';
+import app from '../../src/app';
+
+const request = supertest(app);
+
+describe('Route GET /users', function () {
+  // this.timeout(Infinity);
+
   const defaultUser = {
     id: 1,
-    name: 'Marcos',
+    username: 'marvn',
+    email: 'marcos@email.com',
+    password: '1234',
   };
 
-  describe('Route GET /users', () => {
-    it('deve retorna uma lista de usuarios', (done) => {
-      request.get('/users').end((err, res) => {
-        expect(res.body[0].id).to.be.eql(defaultUser.id);
-        expect(res.body[0].name).to.be.eql(defaultUser.name);
-        done(err);
+  this.beforeAll(async function () {
+    return await conn.init();
+  });
+
+  this.beforeEach((done) => {
+    Users.destroy({ where: {} })
+      .then(() => Users.create(defaultUser))
+      .then(() => {
+        done();
       });
+  });
+
+  it('should return a list of users', function (done) {
+    request.get('/users').end((err, res) => {
+      const body = res.body;
+      console.log(body);
+      expect(body[0].id).to.be.eql(defaultUser.id);
+      expect(body[0].username).to.be.eql(defaultUser.username);
+      expect(body[0].email).to.be.eql(defaultUser.email);
+      expect(body[0].password).to.be.eql(defaultUser.password);
+
+      done(err);
     });
   });
 });
--- test/integration/helpers.js
+++
@@ -1,7 +0,0 @@
-const supertest = require('supertest');
-const chai = require('chai');
-const path = require('path');
-
-global.app = path.resolve(__dirname, '..', '..', 'src', 'app');
-global.request = supertest(app);
-global.expect = chai.expect;
