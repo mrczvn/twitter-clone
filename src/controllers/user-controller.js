7d957c89912edcfb3e6718d49d7d78cb16699bac
Marcos Vinicius <marvn.ants1@gmail.com>
2020-05-09 16:33:56 -0300

    feat: métodos de findOne, update e destroy

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -4,14 +4,15 @@ class UserController {
   constructor() {}
 
   async store(req, res) {
-    const { username, email, password } = req.body;
+    const { id, username, email, password } = req.body;
     try {
       const user = await User.create({
+        id,
         username,
         email,
         password,
       });
-      return res.status(201).json({ result: user });
+      return res.status(201).json(user);
     } catch (error) {
       console.error(error);
       return res.status(400).json(error);
@@ -25,6 +26,45 @@ class UserController {
     } catch (error) {
       console.error(error);
       return res.status(400).json(error);
+    }
+  }
+
+  async findOne(req, res) {
+    const id = req.params;
+    try {
+      const user = await User.findOne({ where: id });
+      return res.status(200).json(user);
+    } catch (error) {
+      console.error(error);
+      return res.status(404).json(error);
+    }
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+    try {
+      const user = await User.update(
+        { username, email, password },
+        {
+          where: { id },
+        }
+      );
+      return res.status(200).json(user);
+    } catch (error) {
+      console.error(error);
+      return res.status(412).json(error);
+    }
+  }
+
+  async destroy(req, res) {
+    const { id } = req.params;
+    try {
+      const user = await User.destroy({ where: { id } });
+      return res.sendStatus(204);
+    } catch (error) {
+      console.error(error);
+      return res.status(412).json(error);
     }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,18 @@
 import Users from './controllers/user-controller';
 
 export default (app) => {
+  // RETORNA A CRIAÇÃO DE UM USER
   app.post('/users', Users.store);
+
+  // RETORNA UMA LISTA DE USERS
   app.get('/users', Users.index);
+
+  // RETORNA UM USER ESPECIFICO
+  app.get('/users/:id', Users.findOne);
+
+  // RETORNA UPDATE DE UM USER
+  app.put('/users/:id', Users.update);
+
+  // DESTROY DE UM USER
+  app.delete('/users/:id', Users.destroy);
 };
