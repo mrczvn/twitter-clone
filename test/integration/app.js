131deeaf960c7ae4cddc26d35426083faddf53b1
Marcos Vinicius <marvn.ants1@gmail.com>
2020-05-11 19:26:05 -0300

    test: refatorando testes

--- test/integration/app.js
+++ test/integration/app.js
@@ -24,93 +24,7 @@ describe('Route /users', function () {
     await Users.create(defaultUser);
   });
 
-  it('should return a list of users', function (done) {
-    request.get('/users').end((err, res) => {
-      const [user] = res.body;
-      const { id, username, email } = user;
-      expect(id).to.be.eql(defaultUser.id);
-      expect(username).to.be.eql(defaultUser.username);
-      expect(email).to.be.eql(defaultUser.email);
-      expect(user).have.property('password');
-
-      done(err);
-    });
-  });
-
-  it('should return a user', function (done) {
-    request.get('/users/1').end((err, res) => {
-      const { id, username, email } = res.body;
-      expect(id).to.eql(defaultUser.id);
-      expect(username).to.be.eql(defaultUser.username);
-      expect(email).to.be.eql(defaultUser.email);
-      expect(res.body).have.property('password');
-
-      done(err);
-    });
-  });
-
-  it('should return a new user', function (done) {
-    const newUser = {
-      id: 2,
-      username: 'mvrtr',
-      email: 'vinicius@email.com',
-      password: '1234',
-    };
-    request
-      .post('/users')
-      .send(newUser)
-      .end((err, res) => {
-        const { id, username, email } = res.body;
-        expect(id).to.be.eq(newUser.id);
-        expect(username).to.be.eql(newUser.username);
-        expect(email).to.be.eql(newUser.email);
-        expect(res.body).have.property('password');
-
-        done(err);
-      });
-  });
-
-  it('should update a user', function (done) {
-    const updateUser = {
-      username: 'mvrtr',
-      email: 'vinicius@email.com',
-      password: '1234',
-    };
-    request
-      .put('/users/1')
-      .send(updateUser)
-      .end((err, res) => {
-        expect(res.body).to.be.eql([1]);
-
-        done(err);
-      });
-  });
-
-  it('should delete a user', function (done) {
-    request.delete('/users/1').end((err, res) => {
-      expect(res.status).to.be.eql(204);
-      done(err);
-    });
-  });
-
-  it('should log a user', function (done) {
-    const logUser = {
-      id: 2,
-      username: 'mvrtr',
-      email: 'vinicius@email.com',
-      password: '1234',
-    };
-    request
-      .post('/signup')
-      .send(logUser)
-      .end((err, res) => {
-        expect(res.body).have.property('token');
-        expect(res.status).to.be.eql(201);
-        done(err);
-      });
-  });
-
-  it('should signin a user', function (done) {
+  it('deve retornar token para login', function (done) {
     const { username, password } = defaultUser;
     request
       .get('/signin')
@@ -122,3 +36,88 @@ describe('Route /users', function () {
       });
   });
 });
+
+// it('should return a user', function (done) {
+//   request.get('/users/1').end((err, res) => {
+//     const { id, username, email } = res.body;
+//     expect(id).to.eql(defaultUser.id);
+//     expect(username).to.be.eql(defaultUser.username);
+//     expect(email).to.be.eql(defaultUser.email);
+//     expect(res.body).have.property('password');
+
+//     done(err);
+//   });
+// });
+
+// it('should return a new user', function (done) {
+//   const newUser = {
+//     id: 2,
+//     username: 'mvrtr',
+//     email: 'vinicius@email.com',
+//     password: '1234',
+//   };
+//   request
+//     .post('/users')
+//     .send(newUser)
+//     .end((err, res) => {
+//       const { id, username, email } = res.body;
+//       expect(id).to.be.eq(newUser.id);
+//       expect(username).to.be.eql(newUser.username);
+//       expect(email).to.be.eql(newUser.email);
+//       expect(res.body).have.property('password');
+
+//       done(err);
+//     });
+// });
+
+// it('should update a user', function (done) {
+//   const updateUser = {
+//     username: 'mvrtr',
+//     email: 'vinicius@email.com',
+//     password: '1234',
+//   };
+//   request
+//     .put('/users/1')
+//     .send(updateUser)
+//     .end((err, res) => {
+//       expect(res.body).to.be.eql([1]);
+
+//       done(err);
+//     });
+// });
+
+// it('should delete a user', function (done) {
+//   request.delete('/users/1').end((err, res) => {
+//     expect(res.status).to.be.eql(204);
+//     done(err);
+//   });
+// });
+
+// it('should log a user', function (done) {
+//   const logUser = {
+//     id: 2,
+//     username: 'mvrtr',
+//     email: 'vinicius@email.com',
+//     password: '1234',
+//   };
+//   request
+//     .post('/signup')
+//     .send(logUser)
+//     .end((err, res) => {
+//       expect(res.body).have.property('token');
+//       expect(res.status).to.be.eql(201);
+//       done(err);
+//     });
+// });
+
+// it('should signin a user', function (done) {
+//   const { username, password } = defaultUser;
+//   request
+//     .get('/signin')
+//     .auth(username, password)
+//     .end((err, res) => {
+//       expect(res.body).have.property('token');
+//       expect(res.status).to.be.eql(200);
+//       done(err);
+//     });
+// });
